fd7bb7302ca30ff90f89b976acd39647ac721206
Jason Mandozzi <jason.mandozzi@gmail.com>
2020-06-28 19:52:56 -0400

    Initial Commit

---
+++ MealPlan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MealPlan
+//
+//  Created by Jason Mandozzi on 6/28/20.
+//  Copyright © 2020 Jason Mandozzi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
